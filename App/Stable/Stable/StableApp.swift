798238385128e0b398db1c681e1ddcad91a73a9c
Kam Nagra <m@nagra.io>
2025-04-24 08:11:27 +0100

    Added Stable iOS app project

---
+++ App/Stable/Stable/StableApp.swift
@@ -0,0 +1,17 @@
+//
+//  StableApp.swift
+//  Stable
+//
+//  Created by Kam Nagra on 24/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct StableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
